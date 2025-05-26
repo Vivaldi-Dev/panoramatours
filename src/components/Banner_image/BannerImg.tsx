41172f57f675ca2cdab3cede4f75aee9ea1b35c1
Vivaldi-Dev <nobelavivaldi@gmail.com>
2025-05-26 11:06:20 +0200

    add new data

--- src/components/Banner_image/BannerImg.tsx
+++ src/components/Banner_image/BannerImg.tsx
@@ -13,7 +13,7 @@ function BannerImg() {
                     <p>de viagem e novidades da Atlantic Travel.</p>
 
 
-                    <div className='mt-5 flex items-center bg-[#FF8700] py-2 px-2 rounded-[30px] gap-4 w-2/12'>
+                    <div className='mt-5 flex items-center bg-[#ff681a] py-2 px-2 rounded-[30px] gap-4 w-2/12'>
 
                         <div className='w-10 h-10 rounded-full bg-white flex justify-center items-center'>
                             <Image width={20} height={20}
@@ -23,7 +23,7 @@ function BannerImg() {
                             />
                         </div>
 
-                        <p className='font-semibold text-white'>Assinar newsletter</p>
+                        <p className='font-semibold text-[#0871B5]'>Assinar newsletter</p>
                     </div>
 
                 </div>
