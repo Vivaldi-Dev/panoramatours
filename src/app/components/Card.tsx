051ef59ca5cac2ce4ddf865ba922a57a626f393f
Vivaldi-Dev <nobelavivaldi@gmail.com>
2025-05-29 10:35:53 +0200

    fix

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -10,16 +10,25 @@ function Card() {
                 <div className='flex md:grid md:grid-cols-3 gap-6 px-1 w-max md:w-full'>
 
                     {/* Card 1 */}
-                    <div className='relative min-w-[250px] md:min-w-0'>
-                        <div className="absolute top-2 left-2 w-full h-full border-2 border-[#ff681a] rounded-2xl rotate-10 z-0"></div>
-                        <div className="w-[236px] md:w-full h-[300px] sm:min-h-[400px] md:min-h-[200px] rounded-2xl bg-[#D9D9D9] flex flex-col justify-between p-4 relative">
-                            <div className='flex w-15 h-15 justify-center items-center bg-[#0682CF] rounded-full'>
-                                <Image width={30} height={10} src="/icons/vetor.png" alt="" />
+                    <div className='relative'>
+                        <div className="absolute  top-2 left-2 w-full h-full border-2 border-[#ff681a] rounded-2xl rotate-10 z-0"></div>
+
+                        <div className="w-[236px] md:w-full relative  h-[300px] sm:h-full sm:min-h-[400px] md:min-h-[200px] sm:p-2 md:p-2 rounded-2xl bg-[#D9D9D9] flex flex-col justify-between">
+                            <div className=' mb-0 p-4 '>
+                                <div className='flex w-15 h-15 justify-center items-center  bg-[#0682CF] rounded-full '>
+                                    <Image width={30} height={10} src="/icons/vetor.png" alt="" />
+                                </div>
+
+                                <div className=' mt-4 mb-0'>
+                                    <div className='font-bold mb-2 sm:mb-5 text-[12px] sm:text-2xl'>
+                                        <p >Missão</p>
+                                    </div>
+
+                                    <p className="text-sm ">
+                                        Proporcionar serviços turísticos de alta qualidade, intermediando produtos com ética profissional, transparência e respeito às necessidades dos nossos clientes.                                </p>
+                                </div>
                             </div>
-                            <div className='mt-4'>
-                                <h2 className='font-bold mb-2 text-[12px] sm:text-2xl'>Missão</h2>
-                                <p className="text-sm">Proporcionar serviços turísticos de alta qualidade...</p>
-                            </div>
+
                         </div>
                     </div>
 
